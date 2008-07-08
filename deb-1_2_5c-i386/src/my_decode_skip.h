394de0aa57f1968bd81e71d8fc537c5958657731
paul-google <paul-google@mineiro.com>
2008-07-08 23:20:27 +0000

    tag 'deb-1_2_5c-i386' created by fw-revision-svn

---
+++ deb-1_2_5c-i386/src/my_decode_skip.h
@@ -0,0 +1,16 @@
+#ifndef _MY_DECODE_SKIP_H_
+#define _MY_DECODE_SKIP_H_
+
+#ifdef __cplusplus
+extern "C"
+    {
+#endif
+
+int 
+my_ei_skip_term (const char* buf, int* index);
+
+#ifdef __cplusplus
+    }
+#endif
+
+#endif /* _MY_DECODE_SKIP_H_ */
